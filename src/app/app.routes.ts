dd298f49dad3063b005c6a82d736f15313fcfe38
MangayarkarasiS <mangay2000@gmail.com>
2025-07-03 11:10:28 +0530

    forms commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ArrayExComponent } from './day2/array-ex/array-ex.component';
 import { StudviewComponent } from './day3/studview/studview.component';
 import { PagenotfoundComponent } from './day3/pagenotfound/pagenotfound.component';
 import { TempFormComponent } from './day4/temp-form/temp-form.component';
+import { ReactformComponent } from './day4/reactform/reactform.component';
 
 export const routes: Routes = [
     {path:'',component:ArrayExComponent}, 
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path:'product',component:ProductComponent},
     {path:'stud-view/:sid',component: StudviewComponent},
      {path:'LoginForm',component:TempFormComponent},
+      {path:'RForm',component:ReactformComponent},
     {path:'**',component:PagenotfoundComponent}
 
 ];
--- src/app/day4/reactform/reactform.component.ts
+++ src/app/day4/reactform/reactform.component.ts
@@ -1,11 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactform',
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './reactform.component.html',
   styleUrl: './reactform.component.css'
 })
 export class ReactformComponent {
+ userForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
 }
